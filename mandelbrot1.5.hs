b6b914f90371651fe9b1057c0d79920600134189
Varik Valefor <varikvalefor@aol.com>
2022-08-14 17:27:25 -0400

    ni'o la'o .inSet. binxo le sezbanzu fancu

--- mandelbrot1.5.hs
+++ mandelbrot1.5.hs
@@ -31,12 +31,29 @@ drawMandelbrot :: Int
 drawMandelbrot w' h' i t r m = toString $ map inSet numList
   where
   numList = map toComplex $ zip yCoords xCoords
+  inSet a = inMandelbrotSet a t i
   xCoords = [1,2..w]
   yCoords = [1,2..h]
-  inSet c = not $ any ((>= t) . magnitude) $ take i $ iterate ((+c) . (**2)) 0
   toString = init . unlines . chunksOf w' . map (bool ' ' '█')
   toComplex (a,b) = m1 :+ m2
     where
     m1 = fst r + (a / w) * (snd r - fst r)
     m2 = fst m + (b / h) * (snd m - fst m)
   [w, h] = map fromIntegral [w', h'];
+
+-- @inMandelbrotSet t d k@ iff MANDELBROT function of @t@ exceeds @d@
+-- within @k@ iterations.
+inMandelbrotSet :: Complex Double
+                -- ^ @inMandelbrotSet@ determines whether or not this
+                -- number is in the MANDELBROT set.
+                -> Double
+                -- ^ This number is the threshold which is used to
+                -- determine whether or not the MANDELBROT function of
+                -- the first argument tends to infinity.
+                -> Int
+                -- ^ This number is the maximum number of iterations
+                -- which are used to determine whether or not the
+                -- MANDELBROT function of the first argument tends to
+                -- infinity.
+                -> Bool;
+inMandelbrotSet c t i = not $ any ((>= t) . magnitude) $ take i $ iterate ((+c) . (**2)) 0;
